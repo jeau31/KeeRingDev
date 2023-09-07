925e4a640481c27c8ba9e71547ee3e9055261545
jeau31 <jeau31@snu.ac.kr>
2023-09-07 13:59:58 +0900

    develop#1/tomato/map_search_page_add

---
+++ lib/screens/MapView/MapSearch/home_screen.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  void onClick() {
+    print('응애');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.orange.shade100,
+      body: Column(
+        children: [
+          AppBar(
+              backgroundColor: Colors.blue.shade200,
+              elevation: 1,
+              title: const Text('Keering',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ))),
+          Flexible(
+            flex: 7,
+            child: Container(
+              decoration: const BoxDecoration(color: Colors.white),
+            ),
+          ),
+          Flexible(
+            flex: 1,
+            child: Container(
+              decoration: BoxDecoration(color: Colors.orange.shade100),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    children: [
+                      IconButton(
+                        onPressed: onClick,
+                        icon: const Icon(Icons.search),
+                      ),
+                      const Text('검색'),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      IconButton(
+                        onPressed: onClick,
+                        icon: const Icon(Icons.map),
+                      ),
+                      const Text('지도'),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      IconButton(
+                        onPressed: onClick,
+                        icon: const Icon(Icons.qr_code),
+                      ),
+                      const Text('QR 스캔'),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      IconButton(
+                        onPressed: onClick,
+                        icon: const Icon(Icons.restart_alt),
+                      ),
+                      const Text('기록'),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      IconButton(
+                        onPressed: onClick,
+                        icon: const Icon(Icons.person_2_outlined),
+                      ),
+                      const Text('내 정보'),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
